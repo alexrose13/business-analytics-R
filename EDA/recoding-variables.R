351b0f4d9cde164296fdb34eeb5acbda301e9887
Alexander Rose <36464519+alexrose13@users.noreply.github.com>
2019-01-26 22:45:59 -0700

    Create recoding-variables.R

---
+++ EDA/recoding-variables.R
@@ -0,0 +1,16 @@
+#Anytime we are working to run an analysis want to ensure that our data set is as clean as possible. Sometimes this may require us to either recode the missing value for a given variable or create an entirely new variable altogether
+#One rather simple way to accomplish this is to lean on the ifelse() command
+
+#Basic examples of ifelse() for variable creation.
+#Practice: Comedy and before 2015
+movies_1017$var1 <- ifelse(movies_1017$genre=="Comedy" &    movies_1017$year<2015,1,0)
+#Practice: Comedy and Rated-R
+movies_1017$var2 <- ifelse(movies_1017$genre=="Comedy" & movies_1017$mpaa=="R",1,0)
+#rated R
+movies_1017$var3 <- ifelse(movies_1017$mpaa=="R",1,0)
+#After 2015 or rated R
+movies_1017$var4 <- ifelse(movies_1017$mpaa=="R" & movies_1017$year>2016,1,0)
+
+#Here we see a more complex example that introduces a logical (or boolean) operator that enables us to make more sophisticated logical statements.
+movies_1017$comedy <- ifelse(movies_1017$genre=="Comedy" | movies_1017$genre=="Romantic Comedy",1,0)
+View(movies_1017)
