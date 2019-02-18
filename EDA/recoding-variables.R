1950747d6ce395b469209e3f8b898f5ad50a4b21
Alexander Rose <36464519+alexrose13@users.noreply.github.com>
2019-02-17 22:41:57 -0700

    Update recoding-variables.R

--- EDA/recoding-variables.R
+++ EDA/recoding-variables.R
@@ -1,4 +1,5 @@
 #Anytime we are working to run an analysis want to ensure that our data set is as clean as possible. Sometimes this may require us to either recode the missing value for a given variable or create an entirely new variable altogether
+
 #One rather simple way to accomplish this is to lean on the ifelse() command
 
 #Basic examples of ifelse() for variable creation.
@@ -14,3 +15,5 @@ movies_1017$var4 <- ifelse(movies_1017$mpaa=="R" & movies_1017$year>2016,1,0)
 #Here we see a more complex example that introduces a logical (or boolean) operator that enables us to make more sophisticated logical statements.
 movies_1017$comedy <- ifelse(movies_1017$genre=="Comedy" | movies_1017$genre=="Romantic Comedy",1,0)
 View(movies_1017)
+
+#While recoding certain variables as 0,1 works well for binary nominal variables, some thought should be given to the recoding of nominal variables that are not binary. In a future version of this post we will explore how to create indicator or "dummy" variables that help us conduct similar analyses with variables that have more than two levels.
